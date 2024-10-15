53c11962b0a6efeed989ee5f5325a920b1b63599
Gonzalx-L <gonzalo.lozano.s.11@gmail.com>
2024-10-15 14:25:24 -0500

    funtion Mul

--- FuncionesBasicas/funtions.js
+++ FuncionesBasicas/funtions.js
@@ -5,7 +5,6 @@ const btn = document.getElementById("Calcular");
 function cualEsMayor() {
   const numero_1 = Number(document.getElementById("numero_1").value);
   const numero_2 = Number(document.getElementById("numero_2").value);
-
   resultado.textContent = "";
   if (numero_1 > numero_2) {
     resultado.textContent = "El número 1 es mayor.";
@@ -16,4 +15,18 @@ function cualEsMayor() {
     resultado.textContent = "Los números son iguales.";
   }
 }
-btn.addEventListener("click", cualEsMayor);
+
+function multiplicacion() {
+  const numero_1 = Number(document.getElementById("numero_1").value);
+  const numero_2 = Number(document.getElementById("numero_2").value);
+  const resM = numero_1 * numero_2;
+  resultado.textContent = "";
+  if (numero_1 && numero_2 > 0) {
+    resultadoMultiplicacion.textContent = "los numeros son positivos" + resM;
+  }
+  if (numero_1 && numero_2 < 0) {
+    resultadoMultiplicacion.textContent = "los numeros son negativos" + resM;
+  }
+}
+btn.addEventListener("click", cualEsMayor());
+btn.addEventListener("click", multiplicacion());
